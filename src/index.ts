f57b32fc34f83502b521cb036748f5512631c301
reeseo3o <notyaeji@gmail.com>
2025-12-21 20:11:21 +0900

    Clean up index exports and remove obsolete npmignore file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,2 @@
 export { default as Picker } from "./components/Picker";
 export { default as BottomSheet } from "./components/BottomSheet";
-
-// Export types
-export type { ScrollPickerProps } from "./components/Picker";
-export type { BottomSheetProps } from "./components/BottomSheet";
